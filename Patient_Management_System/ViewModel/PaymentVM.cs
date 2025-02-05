881eb0d2bce867ab6174c17905f6be8dc0b48a3a
Charmi Ghaghara <charmighaghara1907@gmail.com>
2025-02-05 14:14:55 +0530

    Payment

--- Patient_Management_System/ViewModel/PaymentVM.cs
+++ Patient_Management_System/ViewModel/PaymentVM.cs
@@ -1,5 +1,4 @@
-﻿using Patient_Management_System.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +7,13 @@ namespace Patient_Management_System.ViewModel
 {
     public class PaymentVM
     {
-            public int Patient_ID { get; set; }
-            public int Payment_ID { get; set; }
-            public decimal Amount { get; set; }
-            public string PaymentMethod { get; set; }
-            public DateTime PaymentDate { get; set; } = DateTime.Now;
-            public string Status { get; set; }
-            
-            public string Remarks { get; set; }
-         
-        }
+        public int Patient_ID { get; set; }
+        public int Payment_ID { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; } = DateTime.Now;
+        public string Status { get; set; }
+
+        public string Remarks { get; set; }
     }
+}
